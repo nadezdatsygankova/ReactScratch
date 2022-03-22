ffef2f30639396febcb5b3c68fe9b25c17f7e882
Nadia Tsygankova <47821694+nadezdatsygankova@users.noreply.github.com>
2022-03-22 07:15:34 -0400

    Update functions.js

--- ScrimbaReact/functions.js
+++ ScrimbaReact/functions.js
@@ -1,25 +1,38 @@
+/**
+Challenge: 
+
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+ */
+
 import React from "react"
 import ReactDOM from "react-dom"
 
 function Page() {
     return (
         <div>
-          <header>
-           <nav>
-            <img src ="./react-logo.png" width="10px"/>
-            <h1> Reasons I'm excited to learn React</h1>
-           </nav>
-        
-         </header>
-         <ol>
-            <li>It's a popular library, so I'll be 
-            able to fit in with the cool kids!</li>
-            <li>I'm more likely to get a job as a developer
-            if I know React</li>
-         </ol>
-         <footer> 
-           <p>© 20xx last name here development. All rights reserved.</p>
-         </footer>
+            <header>
+                <nav>
+                    <img src="./react-logo.png" width="40px" />
+                </nav>
+            </header>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+            <footer>
+                © 2021 Nadia development. All rights reserved.
+            </footer>
         </div>
     )
 }
